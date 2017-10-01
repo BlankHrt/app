4d2fa17452a9095bacd3d6f0551345e401568cb4
liuqinghua <759345232@qq.com>
2017-10-01 17:31:40 +0800

    init home

--- js/components/home/index.js
+++ js/components/home/index.js
@@ -1,46 +1,72 @@
 import React, { Component } from "react";
 import { Image, View, StatusBar } from "react-native";
 
-import { Container, Button, H3, Text, Header, Title, Body, Left, Right } from "native-base";
+import { Container, Button, H3, Text, Footer, Thumbnail, FooterTab, Content, Badge, Icon, Header, Tabs, Tab, Title, Body, Left, Right } from "native-base";
 
 import styles from "./styles";
 
 const launchscreenBg = require("../../../img/launchscreen-bg.png");
 const launchscreenLogo = require("../../../img/logo-kitchen-sink.png");
+const menu = [{ name: '首页' }, { name: '活动' }, { name: '驿站' }]
 
 class Home extends Component {
 	// eslint-disable-line
+	constructor(props) {
+		super(props);
+		this.state = {
+			menuName: menu[0].name,
+			active: [true, false, false]
+		}
+	}
+
+	onButtonPress(num) {
+		this.setState(() => {
+			let active = [false, false, false];
+			active[num] = true;
+			return { menuName: menu[num].name, active: active };
+		})
+	}
 
 	render() {
 		return (
 			<Container>
 				<StatusBar barStyle="light-content" />
-				<Image source={launchscreenBg} style={styles.imageContainer}>
-					<View style={styles.logoContainer}>
-						<Image source={launchscreenLogo} style={styles.logo} />
-					</View>
-					<View
-						style={{
-							alignItems: "center",
-							marginBottom: 50,
-							backgroundColor: "transparent",
-						}}
-					>
-						<H3 style={styles.text}>App to showcase</H3>
-						<View style={{ marginTop: 8 }} />
-						<H3 style={styles.text}>NativeBase components</H3>
-						<View style={{ marginTop: 8 }} />
-					</View>
-					<View style={{ marginBottom: 80 }}>
+				<Header>
+					<Left>
 						<Button
-							style={{ backgroundColor: "#6FAF98", alignSelf: "center" }}
+							transparent
 							onPress={() => this.props.navigation.navigate("DrawerOpen")}
 						>
-							<Text>Lets Go!</Text>
+							<Thumbnail small source={launchscreenBg} />
 						</Button>
-					</View>
-				</Image>
-			</Container>
+					</Left>
+					<Body>
+						<Title>{this.state.menuName}</Title>
+					</Body>
+					<Right />
+				</Header>
+				<Content>
+					<Text>{this.state.menuName}</Text>
+				</Content>
+				<Footer>
+					<FooterTab>
+						<Button active={this.state.active[0]} badge vertical onPress={() => this.onButtonPress(0)}>
+							<Badge><Text>2</Text></Badge>
+							<Icon name="apps" />
+							<Text>首页</Text>
+						</Button>
+						<Button active={this.state.active[1]} vertical onPress={() => this.onButtonPress(1)}>
+							<Icon name="camera" />
+							<Text>活动</Text>
+						</Button>
+						<Button active={this.state.active[2]} badge onPress={() => this.onButtonPress(2)}>
+							<Badge ><Text>51</Text></Badge>
+							<Icon name="navigate" />
+							<Text>驿站</Text>
+						</Button>
+					</FooterTab>
+				</Footer>
+			</Container >
 		);
 	}
 }
